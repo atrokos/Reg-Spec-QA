07d52ab85f5a28786059a7d590ff004b82767284
Ondřej Boška <ondraboska@gmail.com>
2025-04-28 09:58:41 +0200

    dvoraa: Update evaluation script

--- eval.py
+++ eval.py
@@ -1,27 +1,52 @@
 import os
 import argparse
 import pandas as pd
+
 from bert_score import score
 
+parser = argparse.ArgumentParser(description="Evaluate a metric (e.g., BERTScore) on a CSV file with target and predicted columns.")
+parser.add_argument("--input_csv",type=str, required=True, help="Path to the input CSV file containing 'target' and 'predicted' columns.",)
+parser.add_argument("--metric", type=str, required=True, help="The name of the metric to compute (e.g., 'bertscore').",)
+parser.add_argument("--summarize", type=str, default=False, required=False, help="",)
 
-def evaluate_metric(
+
+
+def evaluate(
     metric_name: str,
     df: pd.DataFrame,
     target_column: str = "answer",
     predicted_column: str = "predicted_answer",
     model_type: str = "roberta-large_L2",
-    lang: str = "en",
+    lang: str = "cz",
 ) -> pd.DataFrame:
-    if metric_name.lower() == "bertscore":
-        references = df[target_column].tolist()
-        candidates = df[predicted_column].tolist()
+    if metric_name.lower() == "bert_score":
+        # Throw away those rows with ERROR in answer
+        df_cleaned = df[~df['predicted_answer'].str.contains("ERROR", na=False)]
+        split_type = df_cleaned.pop("split_type")
+        df_cleaned.insert(0, "split_type", split_type)
+        image_url = df_cleaned.pop("image_url")
+        df_cleaned.insert(8, "image_url", image_url)
 
-        P, R, F1 = score(candidates, references, model_type=model_type, lang=lang)
+        # Result for predicted_answer (cz question)
+        references = df_cleaned[target_column].tolist()
+        candidates = df_cleaned["predicted_answer"].tolist()
 
-        df[f"metric_{metric_name}_F1"] = F1.tolist()
-        df[f"metric_{metric_name}_P"] = P.tolist()
-        df[f"metric_{metric_name}_R"] = R.tolist()
-        return df
+        P, R, F1 = score(candidates, references, lang=lang)
+
+        df_cleaned.insert(5, f"metric_{metric_name}_F1_cz", [round(val, 4) for val in F1.tolist()])
+        df_cleaned.insert(6, f"metric_{metric_name}_P_cz", [round(val, 4) for val in P.tolist()])
+        df_cleaned.insert(7, f"metric_{metric_name}_R_cz", [round(val, 4) for val in R.tolist()])
+
+        # Result for predicted_answer_en (question in english)
+        candidates = df_cleaned["predicted_answer_en"].tolist()
+
+        P, R, F1 = score(candidates, references, lang=lang)
+        
+        df_cleaned.insert(9, f"metric_{metric_name}_F1_en", [round(val, 4) for val in F1.tolist()])
+        df_cleaned.insert(10, f"metric_{metric_name}_P_en", [round(val, 4) for val in P.tolist()])
+        df_cleaned.insert(11, f"metric_{metric_name}_R_en", [round(val, 4) for val in R.tolist()])
+
+        return df_cleaned
     else:
         raise ValueError(
             f"Metric '{metric_name}' is not supported. Currently, only 'bertscore' is implemented."
@@ -29,26 +54,10 @@ def evaluate_metric(
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Evaluate a metric (e.g., BERTScore) on a CSV file with target and predicted columns."
-    )
-    parser.add_argument(
-        "--input_csv",
-        type=str,
-        required=True,
-        help="Path to the input CSV file containing 'target' and 'predicted' columns.",
-    )
-    parser.add_argument(
-        "--metric",
-        type=str,
-        required=True,
-        help="The name of the metric to compute (e.g., 'bertscore').",
-    )
-
     args = parser.parse_args()
 
     df = pd.read_csv(args.input_csv)
-    df_result = evaluate_metric(args.metric, df)
+    df_result = evaluate(args.metric, df)
 
     output_path = os.path.join(os.path.dirname(args.input_csv), "eval_results")
     os.makedirs(output_path, exist_ok=True)
@@ -60,5 +69,8 @@ if __name__ == "__main__":
     df_result.to_csv(output_csv, index=False)
 
     print(
-        f"Evaluation complete. The updated CSV with the '{args.metric}' metric has been saved to '{args.output_csv}'."
+        f"Evaluation complete. The updated CSV with the '{args.metric}' metric has been saved to '{output_csv}'."
     )
+
+    if args.summarize:
+        pass
